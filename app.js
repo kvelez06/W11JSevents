d0a0528aee3ebd751d8c94cc81b76a05a7ae7c28
Karl <heroware.06@hotmail.com>
2020-09-01 20:49:21 -0600

    added click, hover, and keypress

---
+++ app.js
@@ -0,0 +1,36 @@
+// function makeBold() {
+//     headerElement.style.fontWeight = "bold";
+// }
+
+var headerElement = document.getElementById("header");
+headerElement.addEventListener("click", function() {
+    document.getElementById("header").innerHTML = '<img width="250" height="150" src="https://i.ytimg.com/vi/3XcGpCw1w5k/maxresdefault.jpg" alt="">';
+});
+
+var headerElement2 = document.getElementById("header2");
+headerElement2.addEventListener("mouseover", function() {
+    headerElement2.style.fontFamily = "Impact";
+    headerElement2.style.color = "red";
+});
+
+// var headerElement3 = document.getElementById("header3");
+// headerElement3.addEventListener('keypress', function(eventDetails) {
+//     if(eventDetails.key == " ") {
+//         headerElement3.style.backgroundColor = "red";
+//     }
+// })
+
+// var headerElement3 = document.getElementById("header3");
+// headerElement3.addEventListener('keypress', function(eventDetails) {
+//     if(eventDetails.key == " ") {
+//         console.log(eventDetails)
+//         headerElement3.style.backgroundColor = "green";
+//     }
+// });
+
+var bodyElement = document.getElementById("body");
+bodyElement.addEventListener('keypress', function(eventDetails) {        // "keydown"/"keypress" a key on keyboard has been pressed
+    if(eventDetails.key == " ") {
+        bodyElement.style.backgroundColor = "grey";
+    }
+});
